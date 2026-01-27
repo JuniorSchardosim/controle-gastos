b9c3d8bbadce300a4debf27e6f4791d49dbd8600
JuniorSchardosim <114673319+JuniorSchardosim@users.noreply.github.com>
2026-01-26 23:16:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -585,3 +585,4 @@ function atualizarFuturasResumo() {
     const total = comprasFuturas.reduce((sum, c) => sum + c.valor, 0);
     document.getElementById('total-futuras').textContent = `R$ ${total.toFixed(2)}`;
 }
+
